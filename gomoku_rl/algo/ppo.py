a1fa4d0b2a5f9a3d4e777102e9107649c63705f3
hesic73 <hesicheng2001@163.com>
2023-10-31 21:22:34 +0800

    別管對不對,勉強能跑

--- gomoku_rl/algo/ppo.py
+++ gomoku_rl/algo/ppo.py
@@ -30,26 +30,25 @@ from torchrl.modules import ProbabilisticActor
 from torch.distributions.categorical import Categorical
 
 from torchrl.modules.models import ConvNet,MLP
+from torchrl.modules import ValueOperator
 
 
 class Obs2Logits(nn.Module):
     def __init__(self,device:_device_t,n_action,cnn_kwargs:dict,mlp_kwargs:dict) -> None:
-        super().__init__(device=device)
+        super().__init__()
         self.features=ConvNet(device=device,**cnn_kwargs)
         self.advantage=MLP(out_features=n_action, device=device, **mlp_kwargs)
         
     def forward(self,x:torch.Tensor)->torch.Tensor:
         x = self.features(x)
         advantage = self.advantage(x)
-        logits=torch.special.logit(advantage)
+        logits=torch.special.logit(advantage,eps=1e-6)
         return logits
 
 
 def make_actor(
     cfg: DictConfig,
     action_spec: TensorSpec,
-    observation_key: NestedKey,
-    action_space_size: int,
     device:_device_t,
 ):
     cnn_kwargs = OmegaConf.to_container(cfg.cnn_kwargs)
@@ -60,12 +59,49 @@ def make_actor(
     mlp_kwargs.update(
         {"activation_class": getattr(nn, mlp_kwargs.get("activation_class", "ReLU"))}
     )
-    
-    features=ConvNet(device=device,**cnn_kwargs)
 
-    net = DuelingCnnDQNet(action_space_size, 1, cnn_kwargs, mlp_kwargs)
+    actor_net=Obs2Logits(device=device,n_action=action_spec.space.n,cnn_kwargs=cnn_kwargs,mlp_kwargs=mlp_kwargs)
     
-    policy_module=TensorDictModule(module=nn.Sequential(net,))
+    policy_module=TensorDictModule(module=actor_net,in_keys=['observation'],out_keys=[
+        'logits'
+    ])
     
-    return actor
+    policy_module = ProbabilisticActor(
+    module=policy_module,
+    spec=action_spec,
+    in_keys=["logits"],
+    distribution_class=Categorical,
+    return_log_prob=True,
+)
     
+    return policy_module
+
+
+
+
+def make_critic(
+    cfg: DictConfig,
+    device:_device_t,
+):
+    cnn_kwargs = OmegaConf.to_container(cfg.cnn_kwargs)
+    cnn_kwargs.update(
+        {"activation_class": getattr(nn, cnn_kwargs.get("activation_class", "ReLU"))}
+    )
+    mlp_kwargs = OmegaConf.to_container(cfg.mlp_kwargs)
+    mlp_kwargs.update(
+        {"activation_class": getattr(nn, mlp_kwargs.get("activation_class", "ReLU"))}
+    )
+
+    value_net=nn.Sequential(ConvNet(device=device,**cnn_kwargs),MLP(out_features=1,device=device,**mlp_kwargs))
+    
+    value_module = ValueOperator(
+    module=value_net,
+    in_keys=["observation"],
+)
+    
+    return value_module
+
+
+
+
+        
