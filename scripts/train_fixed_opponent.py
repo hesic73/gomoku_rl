902f0395a5c2cb6dc2076cffadac1fba15956fc4
hesic73 <hesicheng2001@163.com>
2023-11-24 18:09:39 +0800

    some progress

--- scripts/train_fixed_opponent.py
+++ scripts/train_fixed_opponent.py
@@ -58,14 +58,38 @@ def main(cfg: DictConfig):
         opponent = uniform_policy
         logging.info("Opponent: random.")
 
+    baseline_path = os.path.join(
+        "pretrained_models", f"{cfg.board_size}_{cfg.board_size}", "baseline.pt"
+    )
+    if os.path.isfile(baseline_path):
+        baseline = get_policy(
+            name=cfg.algo.name,
+            cfg=cfg.algo,
+            action_spec=env.action_spec,
+            observation_spec=env.observation_spec,
+            device=env.device,
+        )
+        baseline.load_state_dict(torch.load(baseline_path))
+
+        logging.info(f"Baseline: {baseline_path}.")
+    else:
+        baseline = uniform_policy
+        logging.info("Baseline: random.")
+
     if checkpoint := cfg.get("checkpoint", None):
         player.load_state_dict(torch.load(checkpoint))
 
     epochs: int = cfg.get("epochs")
     rounds: int = cfg.get("rounds")
     log_interval: int = cfg.get("log_interval")
+    save_interval: int = cfg.get("save_interval", -1)
     color: str = cfg.get("color").lower()
     assert color in ("white", "black")
+    run_dir = cfg.get("run_dir", None)
+    if run_dir is None:
+        run_dir = run.dir
+    os.makedirs(run_dir, exist_ok=True)
+    logging.info(f"run_dir:{run_dir}")
 
     if color == "white":
         player_black = opponent
@@ -94,15 +118,27 @@ def main(cfg: DictConfig):
                 "eval/black_vs_white": eval_win_rate(
                     env, player_black=player_black, player_white=player_white
                 ),
+                "eval/player_vs_baseline": eval_win_rate(
+                    env, player_black=player, player_white=baseline
+                ),
+                "eval/baseline_vs_player": eval_win_rate(
+                    env, player_black=baseline, player_white=player
+                ),
             }
         )
         if i % log_interval == 0:
             print(
-                "Black vs White:{:.2f}%".format(
+                "Black vs White:{:.2f}%\tPlayer vs Baseline:{:.2f}%,\tBaseline vs Player:{:.2f}%".format(
                     info["eval/black_vs_white"] * 100,
+                    info["eval/player_vs_baseline"] * 100,
+                    info["eval/baseline_vs_player"] * 100,
                 )
             )
 
+        if i % save_interval == 0 and save_interval > 0:
+            torch.save(
+                player.state_dict(), os.path.join(run_dir, f"player_{color}_{i:4d}.pt")
+            )
         run.log(info)
 
         pbar.set_postfix(
@@ -111,7 +147,7 @@ def main(cfg: DictConfig):
             }
         )
 
-    torch.save(player.state_dict(), os.path.join(run.dir, f"player_{color}.pt"))
+    torch.save(player.state_dict(), os.path.join(run_dir, f"player_{color}_final.pt"))
 
 
 if __name__ == "__main__":
