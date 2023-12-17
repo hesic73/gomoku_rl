c32c3ff90248cff3f17651f200c7ca9dff522b0f
hesic73 <hesicheng2001@163.com>
2023-12-17 22:00:32 +0800

    pyproject.toml

--- gomoku_rl/__init__.py
+++ gomoku_rl/__init__.py
@@ -1,4 +1,6 @@
 import os
 
-CONFIG_PATH = os.path.join(os.path.dirname(__file__), os.path.pardir, "cfg")
+CONFIG_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.path.pardir, "cfg")
+)
 from .env import GomokuEnv
